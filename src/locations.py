6fe69b3d76941c50bb335afcad47513b64778a13
LukeRule <47221001+LukeRule@users.noreply.github.com>
2020-12-09 19:52:21 -0600

    Removed doors

--- src/locations.py
+++ src/locations.py
@@ -1,39 +1,19 @@
 coordinates = {
     "Atrium": (3, -13),
-    "Atrium Door 1": (13.26, -10.26),
-    "Atrium Door 2": (-7.3, -10.26),
     "CS Office": (11.51, -5.83),
-    "CS Office Door": (14.63, -7.77),
     "East Room": (6, 4),
-    "East Room Door 1": (11.5, 0),
-    "East Room Door 2": (1.6, 0),
     "West Room": (-4.4, 4),
-    "West Room Door 1": (-0.6, 0),
-    "West Room Door 2": (-8, 0),
     "Electrical Lab": (0, 6),
-    "Electrical Lab Door 1": (5.65, -4),
-    "Electrical Lab Door 2": (-4.4, 4.1),
     "CS Department": (-16.2, 0),
-    "CS Department Door": (-11.75, -1.3),
 }
 
 adjacency = {
-    "Atrium" : [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "Atrium Door 1": [1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-    "Atrium Door 2": [1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-    "CS Office": [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "CS Office Door": [0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-    "East Room": [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    "East Room Door 1": [0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1],
-    "East Room Door 2": [0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-    "West Room": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    "West Room Door 1": [0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
-    "West Room Door 2": [0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
-    "Electrical Lab": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-    "Electrical Lab Door 1": [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    "Electrical Lab Door 2": [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1],.
-    "CS Department": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    "CS Department Door": [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0]
+    "Atrium" : [0, 1, 2, 2, 1, 3],
+    "CS Office": [1, 0, 1, 2, 2, 3],
+    "East Room": [2, 1, 0, 1, 1, 2],
+    "West Room": [2, 2, 1, 0, 1, 1],
+    "Electrical Lab": [1, 2, 1, 1, 0, 2],
+    "CS Department": [3, 3, 2, 1, 2, 0],
 }
 
 import os
